a2155ba281f256d8a931a2ba1bf94cf4d92afe93
mallorysmith64 <mallorysmith64@gmail.com>
2024-05-16 15:18:48 -0400

    user can create todos with names and descriptions with multiple words and phrases, due_date stored initially in memory

--- notes/module_1/week_2/todo.c
+++ notes/module_1/week_2/todo.c
@@ -8,8 +8,8 @@
 struct task
 {
     int task_id;
-    char task_name[20];
-    char task_description[50];
+    char task_name[100];
+    char task_description[200];
     char due_date[20];
     int *ptr;
 };
@@ -25,7 +25,7 @@ struct task *newTask(int id, char name[], char description[], char due_date[])
     }
     else
     {
-        printf("Malloc works\n");
+        printf("Malloc works");
         ptr->task_id = id;
         strcpy(ptr->task_name, name);
         strcpy(ptr->task_description, description);
@@ -34,7 +34,8 @@ struct task *newTask(int id, char name[], char description[], char due_date[])
         printf("%p\n%p\n", ptr, &ptr);
         printf("Name: %s\n", name);
         printf("Description: %s\n", description);
-        printf("Due Date: %s\n", due_date);
+        printf("Due Date: %s", due_date);
+
         free(ptr);
     }
     return ptr;
@@ -53,18 +54,23 @@ int main()
     while (true)
     {
         printf("\nYour response: ");
-        scanf("%s", choice);
+        fgets(choice, 100, stdin);
+        sscanf(choice, "%s", choice);
 
         if (strcmp(choice, "c") == false)
         {
-            printf("Enter task name: \n");
-            scanf("%s", todo.task_name);
+            printf("Enter task name: ");
+            fgets(todo.task_name, sizeof(todo.task_name), stdin);
+            sscanf(todo.task_name, "%99[^\n]", todo.task_name);
 
-            printf("Enter task description: \n");
-            scanf("%s", todo.task_description);
+            printf("Enter task description: ");
+            fgets(todo.task_description, sizeof(todo.task_description), stdin);
+            sscanf(todo.task_description, "%99[^\n]", todo.task_description);
 
-            printf("Enter due date: \n");
-            scanf("%s", todo.due_date);
+            printf("Enter due date: ");
+            fgets(todo.due_date, sizeof(todo.due_date), stdin);
+            sscanf(todo.due_date, "%s", todo.due_date);
+
             newTask(todo.task_id, todo.task_name, todo.task_description, todo.due_date);
         }
 
