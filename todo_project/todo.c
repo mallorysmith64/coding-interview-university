cf6bdad224f125f422303c0a1c40cbf17730c8a9
mallorysmith64 <mallorysmith64@gmail.com>
2024-06-12 22:49:15 -0400

    1. made the defined size of task arrays drier
    2. defined destination file dynamically
    
    3. removed unnecessary data members from struct
    
    4. combined malloc and realloc functions to reduce redundancy,
    no longer passing each field individually to function -
    instead using pointer of a pointer "tasks" to do memory allocations AND
    added counter for the number of tasks being allocated and reallocated
    
    5. rewrote write to file function to write to opened file instead of appending,
    added dynamic error handling to include file name if error occurs,
    added loop for writing to the file,
    when new tasks are added - the user gets a message with the number of tasks
    currently on their todolist
    
    6. when reading the file, the id increments for each new task AND keeps track of
    past increments
    
    7. added back in the logic for quitting the program when the user wants
    
    8. added logic for when user inputs invalid choice such as an option not listed
    in the provided menu
    
    9. Made the program loop, so the user can keep creating tasks, reading tasks,
    etc
    
    10. Debugged vast majority of memory leaks - may be some minor ones left

--- todo_project/todo.c
+++ todo_project/todo.c
@@ -4,153 +4,222 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+
+#define TASK_NAME_SIZE 100
+#define TASK_DESCRIPTION_SiZE 200
+#define TASK_DUE_DATE 20
+
+#define TODO_FILE "todolist.txt"
 
 struct task
 {
-    int task_id;
-    char task_name[100];
-    char task_description[200];
-    // char due_date[20];
-    char *ptr;
-    char *ptr2;
+    int id;
+    char task_name[TASK_NAME_SIZE];
+    char task_description[TASK_DESCRIPTION_SiZE];
+    char due_date[TASK_DUE_DATE];
 };
 
-// create initial memory for tasks
-struct task static *initialTask(int id, char name[], char description[])
+static void dumpTasks(struct task *tasks, int ntasks)
 {
-    struct task *ptr = malloc(10 * sizeof(struct task));
-
-    if (ptr == NULL)
+    for (int i = 0; i < ntasks; i++)
     {
-        printf("Malloc failed");
-        abort();
+        for (int j = 0; j < sizeof(struct task); j++)
+        {
+            char *ptr = (char *)(tasks + i);
+            printf("%d ", ptr[j]);
+        }
+        printf("\n\n");
     }
-    else
-    {
-        printf("Malloc successful");
-        struct task *t;
-        // memset(&t, 0, sizeof(t));
-        t->task_id;
-        // strncpy(ptr->task_name, name, 100);
-        strcpy(ptr->task_name, name);
-        strcpy(ptr->task_description, description);
-        // strcpy(ptr->due_date, due_date);
-
-        printf("\nName: %s\n", name);
-        printf("Description: %s\n", description);
-        // printf("Due Date: %s\n", due_date);
-
-        // free(ptr);
-    }
-    return ptr;
+    printf("\n\n");
 }
 
 // realloc memory for tasks
-void static resizeTask(struct task **ptr, int id, char name[], char description[])
+void static manageTaskArray(struct task **tasks, int *ntasks)
 {
-    struct task *ptr2 = realloc(*ptr, 40 * sizeof(struct task));
-
-    if (ptr2 == NULL)
+    // create initial memory for tasks
+    if (*tasks == NULL)
     {
-        printf("Realloc failed");
-        abort();
+        printf("manageTaskArray(): line %d, *ptr was NULL\n", __LINE__);
+        printf("manageTaskArray(): line %d, ntasks was %d\n", __LINE__, *ntasks);
+
+        *ntasks = 1;
+        printf("manageTaskArray(): line %d, ntasks was %d\n", __LINE__, *ntasks);
+        *tasks = malloc(*ntasks * sizeof(struct task));
     }
     else
     {
-        printf("Realloc successful");
-    }
+        printf("manageTaskArray(): line %d, *ptr was %p\n", __LINE__, *tasks);
+        printf("manageTaskArray(): line %d, ntasks was %d\n", __LINE__, *ntasks);
 
-    free(ptr2);
+        (*ntasks)++;
+        *tasks = realloc(*tasks, *ntasks * sizeof(struct task));
+        printf("manageTaskArray(): line %d, *ptr was %p\n", __LINE__, *tasks);
+    }
+    if (tasks == NULL)
+    {
+        printf("Malloc/realloc failed, ntasks = %d\n", *ntasks);
+        exit(1); // abort();
+    }
+    else
+    {
+        dumpTasks(*tasks, *ntasks);
+        printf("Malloc successful\n");
+        printf("sizeof struct task %ld\n", sizeof(struct task));
+        printf("memory address of ptr + 0 %p\n", (void *)*tasks);
+        printf("memory address of ptr + %d %p\n", *ntasks, (void *)(*tasks + *ntasks));
+        memset((*tasks) + (*ntasks - 1), 0, sizeof(struct task));
+
+        // printf("id: %d\n", id);
+        // printf("Name: %s\n", name);
+        // printf("Description: %s\n", description);
+        // printf("Due Date: %s\n", due_date);
+    }
 }
 
 // write tasks to file
-void static createTask(struct task **ptr, int id, char name[], char description[])
+void static writeTasksToDisk(struct task *tasks, int ntasks)
 {
-
-    int i, n = 1;
-    int count = 0;
-
-    for (i = 0; i < n; i++)
+    FILE *fptr = fopen(TODO_FILE, "w");
+    if (!fptr)
     {
+        perror(TODO_FILE);
+        exit(1);
+    }
 
-        FILE *fptr = fopen("todolist.txt", "a");
+    for (int i = 0; i < ntasks; i++)
+    {
+        fprintf(fptr, "%d\t%s\t%s\t%s\n",
+                tasks[i].id,
+                tasks[i].task_name,
+                tasks[i].task_description,
+                tasks[i].due_date);
 
-        fprintf(fptr, "\n%d\t%s\t%s\t", count, name, description);
-        printf("\nCreated task successfully!\n%s\n%s", name, description);
-        count++;
-        fclose(fptr);
+        // printf("\nId: %d\nName: %s\nDescription: %s\nDue_Date: %s\n",
+        //        tasks[i].id,
+        //        tasks[i].task_name,
+        //        tasks[i].task_description,
+        //        tasks[i].due_date);
+    }
+
+    printf("\nCreated task(s) successfully!\nNumber of tasks: %d\n ", ntasks);
+    fclose(fptr);
+}
+
+static void displayTasks(struct task *tasks, int ntasks)
+{
+    for (int i = 0; i < ntasks; i++)
+    {
+        printf("\nId: %d\nName: %s\nDescription: %s\nDue_Date: %s\n",
+               tasks[i].id,
+               tasks[i].task_name,
+               tasks[i].task_description,
+               tasks[i].due_date);
     }
 }
 
 // read all tasks from file
-void static readTask(struct task **ptr, int id, char name[], char description[])
+static int readTasksFromDisk(struct task **tasks, int *ntasks)
 {
-    FILE *fptr = fopen("todolist.txt", "r");
-
+    *tasks = NULL;
+    *ntasks = 0;
+    int highestID = -999999;
+    FILE *fptr = fopen(TODO_FILE, "r");
     if (fptr == NULL)
     {
-        perror("File does not exist");
+        perror(TODO_FILE);
         exit(1);
     }
 
-    puts("\n#\tTask Name:\tDescription:\tDue Date:");
-
-    while (!feof(fptr))
+    while (true)
     {
-        char ch;
-        ch = fgetc(fptr);
-        printf("%c", ch);
+        struct task t;
+        memset(&t, 0, sizeof(t));
+        int r = fscanf(fptr, "%d\t%s\t%s\t%s\n", &t.id, t.task_name, t.task_description, t.due_date);
+
+        if (t.id > highestID)
+            highestID = t.id;
+        if (r < 2) // if we didn't read anywhere near a whole line, we must be at EOF
+            break;
+        manageTaskArray(tasks, ntasks);
+        memcpy((*tasks) + ((*ntasks) - 1), &t, sizeof(t));
+        printf("record number: %d\n", (*ntasks) - 1);
+        dumpTasks(*tasks, *ntasks);
+        (*tasks, *ntasks);
     }
     fclose(fptr);
+    return highestID;
 }
 
 int main()
 {
+    char choice[10];
+    struct task *tasks = NULL; // , *currentTask = NULL;
+    int ntasks = 0;
+    int highwaterId = readTasksFromDisk(&tasks, &ntasks);
+    ;
+    // memset(&todo, 0, sizeof(struct task));
 
-    while (true)
+    while (1)
     {
-        char choice[10];
-        struct task todo;
-        struct task *tasks = NULL;
-        // char ptr;
-
         printf("--------------------------------------\n");
         printf("To add a new task: type 'c'\n");
         printf("To see all current tasks: type 'r'\n");
         printf("To update a task: type 'u'\n");
         printf("To delete a task: type 'd'\n");
+        printf("To quit, type 'q'\n");
         printf("--------------------------------------");
 
         printf("\nYour response: ");
+        memset(choice, 0, sizeof(choice));
         fgets(choice, 9, stdin);
         sscanf(choice, "%s", choice);
 
-        if (strcmp(choice, "c") == 0)
+        if (strcmp(choice, "c") == 0 || strcmp(choice, "C") == 0)
         {
+            char task_name[TASK_NAME_SIZE];
+            char task_description[TASK_DESCRIPTION_SiZE];
+            char task_due_date[TASK_DUE_DATE];
 
             printf("Enter task name: ");
-            fgets(todo.task_name, sizeof(todo.task_name), stdin);
-            sscanf(todo.task_name, "%99[^\n]", todo.task_name);
+            fgets(task_name, sizeof(task_name), stdin);
+            sscanf(task_name, "%99[^\n]", task_name);
 
             printf("Enter task description: ");
-            fgets(todo.task_description, sizeof(todo.task_description), stdin);
-            sscanf(todo.task_description, "%99[^\n]", todo.task_description);
+            fgets(task_description, sizeof(task_description), stdin);
+            sscanf(task_description, "%99[^\n]", task_description);
 
-            // printf("Enter due date: ");
-            // fgets(todo.due_date, sizeof(todo.due_date), stdin);
-            // sscanf(todo.due_date, "%s", todo.due_date);
+            printf("Enter due date: ");
+            fgets(task_due_date, sizeof(task_due_date), stdin);
+            sscanf(task_due_date, "%s", task_due_date);
 
-            initialTask(todo.task_id, todo.task_name, todo.task_description);
-            resizeTask(&tasks, todo.task_id, todo.task_name, todo.task_description);
-            createTask(&tasks, todo.task_id, todo.task_name, todo.task_description);
+            printf("\n");
+
+            manageTaskArray(&tasks, &ntasks);
+            // tasks = initialTask(highwaterId, task_name, task_description, due_date);
+            // resizeTask(&tasks, todo.id, todo.task_name, todo.task_description, todo.due_date);
+            struct task *newTask = tasks + (ntasks - 1);
+            newTask->id = ++highwaterId;
+            strncpy(newTask->task_name, task_name, sizeof(newTask->task_name));
+            strncpy(newTask->task_description, task_description, sizeof(newTask->task_description));
+            strncpy(newTask->due_date, task_due_date, sizeof(newTask->due_date));
+            writeTasksToDisk(tasks, ntasks);
         }
-        else if (strcmp(choice, "r") == 0)
+        else if (strcmp(choice, "r") == 0 || strcmp(choice, "R") == 0)
         {
             printf("succesful read \n");
-            resizeTask(&tasks, todo.task_id, todo.task_name, todo.task_description);
-            readTask(&tasks, todo.task_id, todo.task_name, todo.task_description);
+            displayTasks(tasks, ntasks);
         }
-
-        return 0;
+        else if (strcmp(choice, "q") == 0)
+        {
+            printf("Exiting...\n");
+            break;
+        }
+        else
+        {
+            printf("Invalid choice. Try again\n");
+        }
     }
+    free(tasks);
+    exit(0);
 }
